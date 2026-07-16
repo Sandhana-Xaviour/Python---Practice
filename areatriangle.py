ea64194d78334252fab19652b80862b82b4581cc
Sandhana <scixaviour2022@gmail.com>
2026-07-16 21:08:46 +0530

    New files added

---
+++ areatriangle.py
@@ -0,0 +1,5 @@
+base = float (input ("Enter the base value :"))
+height = float (input ("Enter the height value :"))
+
+area = 0.5 * base * height
+print (f" the area of triangle is {area}")
---
+++ swap.py
@@ -0,0 +1,9 @@
+a = int (input ( "Enter the first number :"))
+b = int (input ( "Enter the second number :"))
+
+print (f"Before swapping the value of a is {a} and b is {b}")
+
+temp = a
+a = b
+b = temp
+print (f"After sswapping the value of a is {a} and b is {b}")
